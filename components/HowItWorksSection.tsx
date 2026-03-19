0daae9187af07912fd46cc467813c0aa6bdd749e
rafeed-iconfitness <rafeediqbal@gmail.com>
2026-03-20 00:29:14 +0600

    Update HowItWorksSection.tsx

--- components/HowItWorksSection.tsx
+++ components/HowItWorksSection.tsx
@@ -173,28 +173,6 @@ function UnderstandVis() {
         />
       </div>
 
-      {/* Lightbulb icon */}
-      <div
-        style={{
-          position: "absolute",
-          top: "12px",
-          right: "12px",
-          animation: "hiw-pulse 3s ease infinite",
-        }}
-      >
-        <svg
-          width="16"
-          height="16"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="var(--accent)"
-          strokeWidth="2"
-          strokeLinecap="round"
-          style={{ opacity: 0.5 }}
-        >
-          <path d="M9 18h6M10 22h4M15.09 14c.18-.98.65-1.74 1.41-2.5A4.65 4.65 0 0 0 18 8 6 6 0 0 0 6 8c0 1 .23 2.23 1.5 3.5A4.61 4.61 0 0 1 8.91 14" />
-        </svg>
-      </div>
     </div>
   );
 }
@@ -336,29 +314,6 @@ function PlanBuildVis() {
         ))}
       </div>
 
-      {/* Gear icon */}
-      <div
-        style={{
-          position: "absolute",
-          top: "12px",
-          right: "12px",
-          animation: "hiw-pulse 3s ease infinite 1s",
-        }}
-      >
-        <svg
-          width="14"
-          height="14"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="var(--accent)"
-          strokeWidth="2"
-          strokeLinecap="round"
-          style={{ opacity: 0.4 }}
-        >
-          <circle cx="12" cy="12" r="3" />
-          <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1-2.83 2.83l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-4 0v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83-2.83l.06-.06A1.65 1.65 0 0 0 4.68 15a1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1 0-4h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 2.83-2.83l.06.06A1.65 1.65 0 0 0 9 4.68a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 4 0v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 2.83l-.06.06A1.65 1.65 0 0 0 19.4 9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 0 4h-.09a1.65 1.65 0 0 0-1.51 1z" />
-        </svg>
-      </div>
     </div>
   );
 }
@@ -493,31 +448,6 @@ function LaunchGrowVis() {
         </div>
       </div>
 
-      {/* Rocket icon */}
-      <div
-        style={{
-          position: "absolute",
-          top: "12px",
-          right: "12px",
-          animation: "hiw-float 3s ease infinite",
-        }}
-      >
-        <svg
-          width="16"
-          height="16"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="var(--accent)"
-          strokeWidth="2"
-          strokeLinecap="round"
-          style={{ opacity: 0.4 }}
-        >
-          <path d="M4.5 16.5c-1.5 1.26-2 5-2 5s3.74-.5 5-2c.71-.84.7-2.13-.09-2.91a2.18 2.18 0 0 0-2.91-.09z" />
-          <path d="m12 15-3-3a22 22 0 0 1 2-3.95A12.88 12.88 0 0 1 22 2c0 2.72-.78 7.5-6 11a22.35 22.35 0 0 1-4 2z" />
-          <path d="M9 12H4s.55-3.03 2-4c1.62-1.08 5 0 5 0" />
-          <path d="M12 15v5s3.03-.55 4-2c1.08-1.62 0-5 0-5" />
-        </svg>
-      </div>
     </div>
   );
 }
